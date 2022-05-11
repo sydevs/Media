7b23359d94572ee5b1cc08bbd6391fcb4f31b523
Hitesh <hitesharmaa@gmail.com>
2022-05-11 13:23:05 -0400

    loaded a random audio file

--- app/controllers/meditations_controller.rb
+++ app/controllers/meditations_controller.rb
@@ -2,7 +2,8 @@ class MeditationsController < ApplicationController
 
   def embed
     # Do something to load data from Bubble's REST API
-    @meditation = BubbleAPI.fetch_meditation(params[:id])
+    #@meditation = BubbleAPI.fetch_meditation(params[:id])
+    @meditation = 'http://meditatenepal.org/guided/media/FirstMeditation.mp3'
     # The @ symbol, means that this variable is an instance variable.
     # In the case of a controller, this means the variable will be passed to the view.
   end
