b511c3b48fe2c0f7edee16e7447c91cb9106b2bc
Hitesh <hitesharmaa@gmail.com>
2022-05-13 16:59:23 -0400

    few experiments

--- app/controllers/concerns/bubble_api.rb
+++ app/controllers/concerns/bubble_api.rb
@@ -3,27 +3,45 @@ require 'httparty'
 ## BUBBLE API
 # This concern wraps requests to the Bubble API
 
-module BubbleAPI
+module BubbleApi
 
   def self.fetch_meditation meditation_id
     # "self." means that this is a class method, not an instance method.
     # Set up the parameters we need
+
+
     parameters = {
       id: meditation_id,
-      key: ENV.fetch('BUBBLE_KEY'),
     }
 
+
     # Set the correct URL for the REST API
-    url = "https://wemeditate.bubbleapps.io/api?#{parameters.to_query}"
+    # url = "https://app.sydevelopers.com/version-test/api/1.1/obj/Meditation/1644108823956x994255784581267500"
+    # url = "https://app.sydevelopers.com/version-test/api/1.1/obj/Meditation%20Frame?Meditation=1644108823956x994255784581267500"
+
+    url = "https://app.sydevelopers.com/version-test/api/1.1/obj/Meditation%20Media"
+    # id 1644108823956x994255784581267500
+    # url  "https://wemeditate.bubbleapps.io/api?#{parameters.to_query}"
 
     # Then we make the request using the HTTParty library
     response = HTTParty.get(url, { headers: { 'Content-Type': 'application/json' }, log_level: :debug })
-    puts "#{url} - #{response.pretty_inspect}"
+    # puts "#{url} - #{response.pretty_inspect}"
 
     # Here's an example of a POST request, if you need to do that instead
     # https://github.com/sydevs/Atlas/blob/d5a2dc7e90c2cfe4e1da91bb02871641b574daa0/app/controllers/concerns/messagebird_api.rb#L13
 
     # Do something with the response
+    # puts 'hitesh6'
+    # puts response
+    data = response["response"]["results"] 
+    # puts response["response"]["results"]
+    # data.each do |item|
+    #   puts item["Image"]
+    #   # puts item["category"]
+    #   # puts item["id"]
+    # end
+    
+    # return data
   end
 
 end
--- app/controllers/meditations_controller.rb
+++ app/controllers/meditations_controller.rb
@@ -1,9 +1,15 @@
 class MeditationsController < ApplicationController
 
+include BubbleApi
+
   def embed
     # Do something to load data from Bubble's REST API
-    #@meditation = BubbleAPI.fetch_meditation(params[:id])
-    @meditation = 'http://meditatenepal.org/guided/media/FirstMeditation.mp3'
+    @meditation = BubbleApi.fetch_meditation(params[:id])
+    puts "yo"  
+    puts @meditation
+
+    @testAudio = 'http://meditatenepal.org/guided/media/FirstMeditation.mp3'
+    # @meditation = 'http://meditatenepal.org/guided/media/FirstMeditation.mp3'
     # The @ symbol, means that this variable is an instance variable.
     # In the case of a controller, this means the variable will be passed to the view.
   end
