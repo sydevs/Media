085fe82909d89cbfdac3a23764cfe4cb07a80ac7
Hitesh <hitesharmaa@gmail.com>
2022-05-18 10:30:20 -0400

    Allow embedding

--- app/controllers/meditations_controller.rb
+++ app/controllers/meditations_controller.rb
@@ -3,6 +3,16 @@ class MeditationsController < ApplicationController
 include BubbleApi
 
   def embed
+
+    @client = Client.find_by_public_key(params[:api_key])
+    return if !params[:api_key].present?
+    raise ActionController::RoutingError.new('Not Found') if @client.nil?
+
+    headers['X-FRAME-OPTIONS'] = "ALLOW ALL"
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'GET'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     # Do something to load data from Bubble's REST API
     @meditation_audio = BubbleApi.fetch_meditation(params[:id])
     # puts "yo"  
@@ -19,5 +29,7 @@ include BubbleApi
 
 
   end
+
+  
 
 end
