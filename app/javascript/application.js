0bf92dd2ac44f390ff27a3f3eb5443af13971324
Hitesh <hitesharmaa@gmail.com>
2022-06-13 13:40:54 -0400

    Videos and images seek well to some extent

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,9 +5,21 @@ let audio, video, image, frames;
 let currentFrame = 0;
 let nextFrameTime = 0;
 
-function updateMedia(curTime) {
+function updateMedia(curTime, seeked) {
     // debugger;
     console.log(currentFrame + " " + nextFrameTime);
+
+    if (seeked) {
+        for (var i = 0; i < frames.length; i++) {
+            if (curTime < frames[i][1]) {
+                currentFrame = i - 1;
+                nextFrameTime = frames[i][1];
+                break;
+            }
+        }
+        console.log("Updated : " + currentFrame + " " + nextFrameTime);
+    }
+
 
     if (currentFrame < frames.length - 1) {
         nextFrameTime = frames[currentFrame + 1][1];
@@ -23,37 +35,48 @@ function updateMedia(curTime) {
         if ((frames[currentFrame][0].substr(frames[currentFrame][0].length - 3) == 'mp4')) {
             if (video.src != frames[currentFrame][0]) {
                 video.style.display = "block";
-                image.style.display = "none";
                 video.src = frames[currentFrame][0];
-                console.log('video src change')
+                let videoToSeek = curTime - frames[currentFrame][1];
+                video.currentTime = videoToSeek;
+                image.style.display = "none";
+                image.src = "";
+                video.load();
+                video.play();
+                console.log('video src change');
             }
         } else {
-            // debugger;
             image.style.display = "block";
             video.style.display = "none";
-            debugger;
-            
             image.src = frames[currentFrame][0];
-            console.log('image src change , image src = ' + frames[currentFrame][0]);
+            // console.log('image src change , image src = ' + frames[currentFrame][0]);
         }
 
     } else {
-        // dont update the image and video until its time
-        if (curTime < nextFrameTime) {
+        // don't update the image and video until its time
+        if ((curTime) < nextFrameTime && !seeked) {
+            console.log(nextFrameTime - curTime + ' returning');
             return
         } else {
             if ((frames[currentFrame][0].substr(frames[currentFrame][0].length - 3) == 'mp4')) {
-                if (video.src != frames[currentFrame][0]) {
+                if (video.src != frames[currentFrame][0] || seeked == true) {
                     video.style.display = "block";
-                    image.style.display = "none";
                     video.src = frames[currentFrame][0];
+                    let videoToSeek = curTime - frames[currentFrame][1];
+                    video.currentTime = videoToSeek;
+                    video.load();
+                    console.log("vudeo paused? " + video.paused)
+                    if(!audio.paused){
+                        video.play();
+                    }
+                    
+                    image.style.display = "none";
+                    image.src = "";
                     console.log('video src change')
                 }
             } else {
-                // debugger;
                 image.style.display = "block";
                 video.style.display = "none";
-                debugger;
+                video.src
                 image.src = frames[currentFrame][0];
                 console.log(image);
                 console.log('image src change, not the first one, image src = ' + frames[currentFrame][0])
@@ -62,12 +85,11 @@ function updateMedia(curTime) {
     }
 }
 
+/* 
 function updateImage(curTime) {
-    // console.log(curTime);
     for (let i = 0; i < frames.length; i++) {
         if (i == (frames.length - 1)) {
             if (curTime >= frames[i][1]) {
-                debugger;
                 if ((frames[i][0].substr(frames[i][0].length - 3) == 'mp4')) {
                     if (video.src != frames[i][0]) {
                         video.style.display = "block";
@@ -75,16 +97,11 @@ function updateImage(curTime) {
                         video.src = frames[i][0];
                         console.log('video src change')
                     }
-
-
                 } else {
                     image.style.display = "block";
                     video.style.display = "none";
                     image.src = frames[i][0];
-
                 }
-
-                // console.log(frames[i][0])
             }
         } else {
             if (curTime > frames[i][1] && curTime < frames[i + 1][1]) {
@@ -96,16 +113,14 @@ function updateImage(curTime) {
                         video.load();
                         video.play();
                     }
-
                 } else {
                     image.src = frames[i][0];
                 }
-                // console.log(frames[i][0])
             }
-
         }
     }
 }
+*/
 
 document.addEventListener("DOMContentLoaded", () => {
     // data is a global variable set from embed.html.slim
@@ -125,11 +140,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // image.src = frames[0][0];
 
-    // audio.addEventListener("seeking", () => { 
-    //     updateImage(audio.currentTime)
-    //  });
-
-
     //  audio.addEventListener("play", () => { 
     //     updateImage(audio.currentTime)
     //  });
@@ -142,12 +152,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
     audio.addEventListener('timeupdate', (event) => {
         // updateImage(audio.currentTime);
-        updateMedia(audio.currentTime);
+        // debugger;
+        updateMedia(audio.currentTime, false);
     });
 
     audio.addEventListener('seeked', (event) => {
         // updateImage(audio.currentTime);
         debugger;
-        updateMedia(audio.currentTime);
+        updateMedia(audio.currentTime, true);
     });
 });
