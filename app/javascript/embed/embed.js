876ca4100aba632efd2145cc234c5b177a20388b
Devindra <devindra.payment@gmail.com>
2022-12-29 11:21:57 +0000

    Animate moving marker

--- app/javascript/embed/embed.js
+++ app/javascript/embed/embed.js
@@ -7,15 +7,20 @@ class SahajMediaEmbed {
     const images = document.querySelectorAll('.sym-media__image-stack > img')
     const time = document.querySelector('.sym-media__time')
     const track = document.querySelector('.sym-media__track')
+    const marker = document.querySelector('.sym-media__marker')
 
     let activeImage = images[0]
 
     audio.ontimeupdate = function() {
       const fraction = audio.currentTime / audio.duration
-      const angle = fraction * 360
+      const degrees = fraction * 360
+      const radians = (fraction * 2 - 0.5) * Math.PI
       let newImage = null
       time.innerText = new Date(audio.currentTime * 1000).toISOString().substring(14, 19)
-      track.style = `background-image: conic-gradient(#1E6C71 ${angle}deg, transparent ${angle}deg)`
+      track.style = `background-image: conic-gradient(#1E6C71 ${degrees}deg, transparent ${degrees}deg)`
+      marker.style.left = `calc(50% + ${50 * Math.cos(radians)}% - 4px)`
+      marker.style.top = `calc(50% + ${50 * Math.sin(radians)}% - 4px)`
+      marker.style.opacity = fraction > 0.995 ? 0 : 1
 
       for (let i in images) {
         if (!newImage || Number(images[i].dataset.seconds) < audio.currentTime) {
