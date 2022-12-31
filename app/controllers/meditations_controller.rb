a598b5b61813b133d9292e27bc13297a681665ab
Devindra <devindra.payment@gmail.com>
2022-12-31 10:15:30 +0000

    Allow iframe embeds

--- app/controllers/meditations_controller.rb
+++ app/controllers/meditations_controller.rb
@@ -1,6 +1,10 @@
 class MeditationsController < ApplicationController
     
   def show
+    headers['Access-Control-Allow-Methods'] = 'GET'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+
     @meditation = Meditation.eager_load(:keyframes, :frames).find(params[:id])
     @keyframes = @meditation.keyframes.as_json(only: %i[id frame_id seconds])
     @preload_count = 5
