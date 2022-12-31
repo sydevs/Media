20fe9a8cee50af83c93cf17cb654a49c333c665f
Devindra <devindra.payment@gmail.com>
2022-12-31 10:19:59 +0000

    Set iframe options

--- app/controllers/meditations_controller.rb
+++ app/controllers/meditations_controller.rb
@@ -1,13 +1,20 @@
 class MeditationsController < ApplicationController
+  after_action :allow_iframe, only: :show
     
   def show
-    headers['Access-Control-Allow-Methods'] = 'GET'
-    headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
 
     @meditation = Meditation.eager_load(:keyframes, :frames).find(params[:id])
     @keyframes = @meditation.keyframes.as_json(only: %i[id frame_id seconds])
     @preload_count = 5
   end
+
+  private
+
+    def allow_iframe
+      response.headers.except! 'X-Frame-Options'
+      response.headers['Access-Control-Allow-Methods'] = 'GET'
+      response.headers['Access-Control-Request-Method'] = '*'
+      response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    end
 
 end
