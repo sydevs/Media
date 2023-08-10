951eeedd2f817f7a167b623ae46137f74fdf5016
Devindra <devindra.payment@gmail.com>
2023-08-10 13:42:56 +0100

    Minor fixes

--- app/models/music.rb
+++ app/models/music.rb
@@ -3,6 +3,8 @@ class Music < ApplicationRecord
   acts_as_taggable_on :tags
   validates_presence_of :title, :credit
 
+  default_scope -> { order(updated_at: :desc) }
+
   def duration
     result = audio&.metadata&.fetch(:duration, nil)
     result ? (result / 60).round : nil
