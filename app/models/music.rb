b803a4a41a5efaab058ca00c2b0897074627dd37
Devindra <devindra.payment@gmail.com>
2023-08-10 13:17:44 +0100

    Add audio field

--- app/models/music.rb
+++ app/models/music.rb
@@ -3,7 +3,7 @@ class Music < ApplicationRecord
   acts_as_taggable_on :tags
 
   def duration
-    result = audio.metadata.fetch(:duration, nil)
+    result = audio&.metadata&.fetch(:duration, nil)
     result ? (result / 60).round : nil
   end
 end
